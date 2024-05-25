77be6094cccfe8628b0f2f2c8a61887e1b7c1e6c
aypwu330 <anthonywu@pershing.com.tw>
2024-05-25 13:04:37 +0800

    tune backend

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -1,16 +1,13 @@
 import React, { useState } from "react";
 import ChatBubble from "./ChatBubble";
 
-// TODO: create a Chat component with ChatBubble rendering the response
 // the image should be conditionally rendered based on the cuisine
 function Chat({ response }) {
   return (
     <div>
       <div className="mt-4 ml-4">
-        <ChatBubble
-          text={response}
-          chefName={"Uncle Roger"}
-        />
+        {/* TODO: loop over the messages (response) that the chatbot provides and render it on the Chat component*/}
+        <ChatBubble text={response} chefName={"Uncle Roger"} />
       </div>
     </div>
   );
--- frontend/src/components/ChatBubble.jsx
+++ frontend/src/components/ChatBubble.jsx
@@ -5,26 +5,26 @@ import React from "react";
 function ChatBubble({ text, chefName }) {
   return (
     <div className="flex items-start gap-2.5">
-      {chefName == "Uncle Roger" ? (
+      {chefName === "Uncle Roger" ? (
         <img
-          class="w-8 h-8 rounded-full"
+          className="w-8 h-8 rounded-full"
           src="https://upload.wikimedia.org/wikipedia/commons/4/4f/Nigel_Ng%2C_2020-11-14_A_%28crop%29.jpg"
           alt="nigel"
         />
       ) : (
         <img
-          class="w-8 h-8 rounded-full"
+          className="w-8 h-8 rounded-full"
           src="https://hips.hearstapps.com/hmg-prod/images/chef-host-gordon-ramsay-in-the-semi-finale-pt-2-3-chef-news-photo-1699984599.jpg?crop=1.00xw:1.00xh;0,0&resize=1200:*"
           alt="gordon"
         />
       )}
-      <div class="flex flex-col w-full max-w-[320px] leading-1.5 p-4 border-gray-300 bg-gray-300 rounded-e-xl rounded-es-xl dark:bg-gray-700">
-        <div class="flex items-center space-x-2 rtl:space-x-reverse">
-          <span class="text-sm font-semibold text-gray-900 dark:text-white">
+      <div className="flex flex-col w-full max-w-[320px] leading-1.5 p-4 border-gray-300 bg-gray-300 rounded-e-xl rounded-es-xl dark:bg-gray-700">
+        <div className="flex items-center space-x-2 rtl:space-x-reverse">
+          <span className="text-sm font-semibold text-gray-900 dark:text-white">
             {chefName}
           </span>
         </div>
-        <p class="text-sm font-serif py-2.5 text-gray-900 dark:text-white">
+        <p className="text-sm font-serif py-2.5 text-gray-900 dark:text-white">
           {text}
         </p>
       </div>
